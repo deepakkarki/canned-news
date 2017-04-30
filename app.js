b3a86a46767c5bb503ec2368cb7c4b9bfe38b5d1
Karl Hughes <khughes.me@gmail.com>
2017-04-30 09:12:03 -0500

    Cleaing up some consts

--- app.js
+++ app.js
@@ -3,38 +3,41 @@ const https = require('https');
 const Feedbin = require('feedbin-nodejs');
 const ShortId = require('shortid');
 
-// Feedbin client
-const username = process.env.FEEDBIN_USERNAME;
-const password = process.env.FEEDBIN_PASSWORD;
-const feedbin = new Feedbin(username, password);
+// Misc constants
+const tagName = "Engineering Blogs";
+const resultsPerPage = 200;
 
-// Yesterday's date in ISO format
-let date = new Date();
+// Instantiate Feedbin client
+const feedbin = new Feedbin(process.env.FEEDBIN_USERNAME, process.env.FEEDBIN_PASSWORD);
+
+// Get Yesterday's date in ISO format
+const date = new Date();
 let since = new Date(date.setDate(date.getDate() - 1)).toISOString();
 
-// Get the taggings and entries
+// Get the 'taggings' and 'entries'
 Promise.all([
   feedbin.taggings.getAll(),
   feedbin.entries.getAll({
     'params': {
       'since': since,
-      'per_page': 200
+      'per_page': resultsPerPage
     }
   })
 ]).then((results) => {
 
   // Get the tags that should be added to this email
   let tags = results[0].filter((tag) => {
-    return tag.name === "Engineering Blogs";
+    return tag.name === tagName;
   });
 
   // Get the entries with an Engineering Blogs tag
   let entries = results[1].filter((entry) => {
     return tags.find((tag) =>  entry.feed_id === tag.feed_id);
   });
+
+  // Summary of results
   console.log("Entries found: " + results[1].length);
   console.log("Entries found (filtered): " + entries.length);
-
 
 });
 
